798df58ff7f17de169299397202e37104e24dd14
Maxime Britto <maxime@dev2a.com>
2017-05-02 14:42:41 +0200

    Initial Commit

---
+++ App_Console/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  App_Console
+//
+//  Created by Maxime Britto on 02/05/2017.
+//  Copyright © 2017 mbritto. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
