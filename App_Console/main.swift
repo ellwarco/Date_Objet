c48d016a4bb2bf4d5ee6792906ef5b9408a165e4
Maxime Britto <maxime@dev2a.com>
2017-05-02 14:58:11 +0200

    - Ajout de la classe Utilisateur pour faire des saisie et les convertir - Ajout d'exemples d'utilisation dans le fichier main.swift

--- App_Console/main.swift
+++ App_Console/main.swift
@@ -1,6 +1,6 @@
 //
 //  main.swift
-//  App_Console
+//  AppConsole
 //
 //  Created by Maxime Britto on 02/05/2017.
 //  Copyright © 2017 mbritto. All rights reserved.
@@ -8,5 +8,17 @@
 
 import Foundation
 
+let texte:String
+texte = Utilisateur.saisirTexte()
 print("Hello, World!")
+
+let age:Int
+age = Utilisateur.saisirEntier()
+print("Age : \(age)")
+
+let note:Double
+note = Utilisateur.saisirReel()
+print("Note : \(note)")
+
+
 
